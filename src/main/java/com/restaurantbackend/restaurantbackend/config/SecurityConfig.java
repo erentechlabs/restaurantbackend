2b39010df0e1e1806812bba569f5dd6dc5756879
Eren <128416666+MasterCockatoo@users.noreply.github.com>
2025-09-24 18:59:22 +0300

    Enhance `SecurityConfig` with role-based access and in-memory user management

--- src/main/java/com/restaurantbackend/restaurantbackend/config/SecurityConfig.java
+++ src/main/java/com/restaurantbackend/restaurantbackend/config/SecurityConfig.java
@@ -4,6 +4,12 @@ package com.restaurantbackend.restaurantbackend.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.Customizer;
@@ -15,8 +21,39 @@ public class SecurityConfig {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll()
-                );
+                        .requestMatchers(
+                                "/api/sessions/start/**",
+                                "/api/orders/**",
+                                "/api/menu/**",
+                                "/api/categories/**"
+                        ).permitAll()
+
+
+                        .requestMatchers(
+                                "/api/sessions/close/**",
+                                "/api/tables/**",
+                                "/api/menu-items/**",
+                                "/api/categories/**"
+                        ).hasRole("ADMIN")
+
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(Customizer.withDefaults());
         return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService users(PasswordEncoder encoder) {
+        UserDetails admin = User.withUsername("owner")
+                .password(encoder.encode("1234"))
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(admin);
     }
 }
